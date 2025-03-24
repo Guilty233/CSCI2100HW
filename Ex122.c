8e04c9a4f86f5acf2c886811bcc03c0a13c9e671
root <root@DESKTOP-N30706R>
2025-03-24 23:28:48 +0800

    add

---
+++ Ex122.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+int main(){
+    short N;
+    size_t len = 256;
+    short sum = 0;
+    freopen("test122.txt", "r", stdin);
+    scanf("%hd\n", &N);
+    char *str = (char *)malloc(256 * sizeof(char));
+    while(N--){
+        getline(&str, &len, stdin);
+        sum = 0;
+        for(int i = 0; i < strlen(str); i++){
+            if(isdigit(str[i])){
+                sum++;
+            }
+        }
+        printf("%hd\n", sum);
+    }
+    free(str);
+    return 0;
+}
